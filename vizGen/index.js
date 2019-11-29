b4c1ec5f98a8f6f8277b0cb298ec90758c78d2d3
Gang Tao <gtao@splunk.com>
2019-11-29 11:41:27 -0800

    add vizGen

---
+++ vizGen/index.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+const Mock = require('mockjs')
+
+process.setMaxListeners(Infinity);
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function atob(data) {
+    return Buffer.from(data).toString('base64');
+}
+
+function generateBarData() {
+    const rows = Mock.mock('@integer(0, 10)');
+    const attr1 = Mock.mock('@word(3, 5)');
+    const attr2 = Mock.mock('@word(3, 5)');
+    const result = [];
+    for (let i = 0; i < rows; i++) {
+        const item = {};
+        item[attr1] = Mock.mock('@date(yyyy)');
+        item[attr2] = Mock.mock('@float(60, 100)');
+        result.push(item);
+    }
+
+    return {
+        grammar: `${attr1}*${attr2}&`,
+        data: result
+    }
+}
+
+async function screenshot(url, name) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    await page.screenshot({
+        path: `./images/${name}.png`
+    });
+
+    await browser.close();
+}
+
+async function generateBarCharts() {
+    const id = uuidv4();
+    const host = 'localhost';
+    const port = 8080;
+    const endpoint = 'viz';
+    const {
+        grammar,
+        data
+    } = generateBarData();
+
+    console.log(data);
+    console.log(grammar);
+
+    //const dataset = 'W3sKICAgICAgICAgIHllYXI6ICcxOTUxJywKICAgICAgICAgIHNhbGVzOiAzOAogICAgICB9LCB7CiAgICAgICAgICB5ZWFyOiAnMTk1MicsCiAgICAgICAgICBzYWxlczogNTIKICAgICAgfSwgewogICAgICAgICAgeWVhcjogJzE5NTYnLAogICAgICAgICAgc2FsZXM6IDYxCiAgICAgIH0sIHsKICAgICAgICAgIHllYXI6ICcxOTU3JywKICAgICAgICAgIHNhbGVzOiAxNDUKICAgICAgfSwgewogICAgICAgICAgeWVhcjogJzE5NTgnLAogICAgICAgICAgc2FsZXM6IDQ4CiAgICAgIH0sIHsKICAgICAgICAgIHllYXI6ICcxOTU5JywKICAgICAgICAgIHNhbGVzOiAzOAogICAgICB9LCB7CiAgICAgICAgICB5ZWFyOiAnMTk2MCcsCiAgICAgICAgICBzYWxlczogMzgKICAgICAgfSwgewogICAgICAgICAgeWVhcjogJzE5NjInLAogICAgICAgICAgc2FsZXM6IDM4CiAgICAgIH1d';
+    const dataset = atob(JSON.stringify(data));
+    console.log(dataset);
+
+    const url1 = `http://${host}:${port}/${endpoint}?sketchify=1&grammar=${grammar}&dataset=${dataset}`;
+    const url2 = `http://${host}:${port}/${endpoint}?sketchify=0&grammar=${grammar}&dataset=${dataset}`;
+
+    await screenshot(url1, `handy_${id}`);
+    await screenshot(url2, `${id}`);
+}
+
+(async () => {
+    for( let i = 0; i < 10 ; i ++ ) {
+        await generateBarCharts();
+    }
+})();
